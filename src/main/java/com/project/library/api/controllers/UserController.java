3eeae8ff2c6b953470003245447c957415ac133d
sountastic <paulina.gr@o2.pl>
2020-06-27 00:29:09 +0200

    Adding userServiceImplementation

--- src/main/java/com/project/library/api/controllers/UserController.java
+++ src/main/java/com/project/library/api/controllers/UserController.java
@@ -1,9 +1,19 @@
 package com.project.library.api.controllers;
 
+import com.project.library.api.controllers.mappers.UserMapper;
+import com.project.library.api.services.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+
+    private final UserServiceImpl userService;
+    private final UserMapper userMapper;
+
+
+
 }
--- src/main/java/com/project/library/api/services/UserServiceImpl.java
+++ src/main/java/com/project/library/api/services/UserServiceImpl.java
@@ -1,7 +1,32 @@
 package com.project.library.api.services;
 
+import com.project.library.api.domain.User;
+import com.project.library.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(final Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(final User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUserById(final Long id) {
+        userRepository.deleteById(id);
+    }
 }
