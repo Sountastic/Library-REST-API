219f5511a28918af0c3ccc0aea371d69ead4eaef
sountastic <paulina.gr@o2.pl>
2020-06-28 17:57:01 +0200

    adding initial data

--- src/main/java/com/project/library/api/bootstrap/DataLoader.java
+++ src/main/java/com/project/library/api/bootstrap/DataLoader.java
@@ -1,11 +1,16 @@
 package com.project.library.api.bootstrap;
 
+import com.project.library.api.domain.*;
 import com.project.library.api.repositories.BookEditionRepository;
 import com.project.library.api.repositories.RentalRepository;
 import com.project.library.api.repositories.TitleRepository;
 import com.project.library.api.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
+import java.util.Calendar;
+import java.util.Date;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -26,8 +31,51 @@ public class DataLoader implements CommandLineRunner {
         this.userRepository = userRepository;
     }
 
+
     @Override
     public void run(String... args) throws Exception {
+        loadData();
+    }
 
+    private void loadData() {
+
+        User user1 = new User("Tom", "Snow", new Date(99, 3, 13));
+        User user2 = new User("Justyna", "Wozniak", new Date(98, 1, 10));
+        userRepository.saveAndFlush(user1);
+        userRepository.saveAndFlush(user2);
+
+        Title title1 = new Title("Harry Potter", "J.K.Rownling", 2001);
+        Title title2 = new Title("Memories", "J.Hiragami", 2010);
+        titleRepository.saveAndFlush(title1);
+        titleRepository.saveAndFlush(title2);
+
+        BookEdition bookEdition1 = new BookEdition(title1, BookStatus.AVAILABLE);
+        BookEdition bookEdition2 = new BookEdition(title1, BookStatus.AVAILABLE);
+        BookEdition bookEdition3 = new BookEdition(title1, BookStatus.BOOKED);
+        BookEdition bookEdition4 = new BookEdition(title2, BookStatus.BORROWED);
+        BookEdition bookEdition5 = new BookEdition(title2, BookStatus.LOST);
+        BookEdition bookEdition6 = new BookEdition(title2, BookStatus.BORROWED);
+        bookEditionRepository.saveAndFlush(bookEdition1);
+        bookEditionRepository.saveAndFlush(bookEdition2);
+        bookEditionRepository.saveAndFlush(bookEdition3);
+        bookEditionRepository.saveAndFlush(bookEdition4);
+        bookEditionRepository.saveAndFlush(bookEdition5);
+        bookEditionRepository.saveAndFlush(bookEdition6);
+
+        Rental rental1 = new Rental(bookEdition4, user1, new Date(2020, 3, 02), new Date(2020, 7, 13));
+        Rental rental2 = new Rental(bookEdition5, user1, new Date(2018, 9, 23), new Date(2019, 1, 11));
+        Rental rental3 = new Rental(bookEdition6, user1, new Date(2019, 11, 30), new Date(2020, 4, 3));
+        rentalRepository.saveAndFlush(rental1);
+        rentalRepository.saveAndFlush(rental2);
+        rentalRepository.saveAndFlush(rental3);
+    }
+
+    @PreDestroy
+    public void removeData() {
+
+        bookEditionRepository.deleteAll();
+        rentalRepository.deleteAll();
+        titleRepository.deleteAll();
+        userRepository.deleteAll();
     }
 }
