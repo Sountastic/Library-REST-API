353155f931be8487dc5bed8920b9d4cf4529d017
sountastic <paulina.gr@o2.pl>
2020-06-28 23:01:10 +0200

    Adding rental controller and service implementation

--- src/main/java/com/project/library/api/controllers/RentalController.java
+++ src/main/java/com/project/library/api/controllers/RentalController.java
@@ -1,9 +1,47 @@
 package com.project.library.api.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.project.library.api.controllers.mappers.RentalMapper;
+import com.project.library.api.domain.dto.RentalDto;
+import com.project.library.api.exceptions.RentalNotFoundException;
+import com.project.library.api.services.RentalServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/v1/rental")
 public class RentalController {
+
+    private final RentalServiceImpl rentalService;
+    private final RentalMapper rentalMapper;
+
+    @GetMapping("/rentals")
+    @ResponseBody
+    public List<RentalDto> getRentals() {
+        return rentalMapper.mapToRentalDtoList(rentalService.getAllRentals());
+    }
+
+    @GetMapping("/{rentalId}")
+    @ResponseBody
+    public RentalDto getRentalById(@PathVariable("rentalId") Long rentalId) throws RentalNotFoundException {
+        return rentalMapper.mapToRentalDto(rentalService.getRentalById(rentalId).orElseThrow(RentalNotFoundException::new));
+    }
+
+    @DeleteMapping("/{rentalId}")
+    public void deleteRental(@PathVariable("rentalId") Long rentalId) {
+        rentalService.deleteRentalById(rentalId);
+    }
+
+    @PutMapping("")
+    public RentalDto updateRental(@RequestBody RentalDto rentalDto) {
+        return rentalMapper.mapToRentalDto(rentalService.saveRental(rentalMapper.matToRental(rentalDto)));
+    }
+
+    @PostMapping("")
+    @ResponseBody
+    public void createRental(@RequestBody RentalDto rentalDto) {
+        rentalService.saveRental(rentalMapper.matToRental(rentalDto));
+    }
 }
--- src/main/java/com/project/library/api/services/RentalServiceImpl.java
+++ src/main/java/com/project/library/api/services/RentalServiceImpl.java
@@ -1,7 +1,31 @@
 package com.project.library.api.services;
 
+import com.project.library.api.domain.Rental;
+import com.project.library.api.repositories.RentalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RentalServiceImpl implements RentalService {
+    @Autowired
+    RentalRepository rentalRepository;
+
+    public List<Rental> getAllRentals() {
+        return rentalRepository.findAll();
+    }
+
+    public Optional<Rental> getRentalById(final Long id) {
+        return rentalRepository.findById(id);
+    }
+
+    public Rental saveRental(final Rental rental) {
+        return rentalRepository.save(rental);
+    }
+
+    public void deleteRentalById(final Long id) {
+        rentalRepository.deleteById(id);
+    }
 }
